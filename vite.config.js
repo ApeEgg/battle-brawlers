12c37f5e0fd8dd736970c55b2d63ba5d9b0fc1a2
kkortes <kim@kortes.se>
2025-09-18 08:48:52 +0200

    replaced sveltekit-autoimport with unplugin-svelte-components

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import autoImport from 'sveltekit-autoimport';
 import tailwindcss from '@tailwindcss/vite';
+import Components from 'unplugin-svelte-components/vite';
 
 export default defineConfig({
   build: {
@@ -14,20 +14,24 @@ export default defineConfig({
   },
   plugins: [
     tailwindcss(),
-    autoImport({
-      components: [{ name: './src/components', flat: true }],
-      mapping: {
-        ENV: `import ENV from '$src/constants/ENV_VARS'`,
-        app: `import app from '$src/app.svelte'`
-      },
-      module: {
-        svelte: ['onMount', 'onDestroy'],
-        '$src/store': ['STORES', 'ACTIONS'],
-        '$src/ts/use': ['tooltip'],
-        'tailwind-merge': ['twMerge as tw']
-      },
-      include: ['**/*.svelte']
+    Components({
+      dirs: ['./src/components'],
+      dts: './src/components.d.ts'
     }),
+    // autoImport({
+    //   components: [{ name: './src/components', flat: true }],
+    //   mapping: {
+    //     ENV: `import ENV from '$src/constants/ENV_VARS'`,
+    //     app: `import app from '$src/app.svelte'`
+    //   },
+    //   module: {
+    //     svelte: ['onMount', 'onDestroy'],
+    //     '$src/store': ['STORES', 'ACTIONS'],
+    //     '$src/ts/use': ['tooltip'],
+    //     'tailwind-merge': ['twMerge as tw']
+    //   },
+    //   include: ['**/*.svelte']
+    // }),
     sveltekit()
   ]
 });
